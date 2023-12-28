7455ece7f44fffd53274aff7ae2d4357933b6558
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2023-12-29 00:21:31 +0530

    Implement dribbling with ball sticking to feet

--- Assets/Scripts/KickBall.cs
+++ Assets/Scripts/KickBall.cs
@@ -11,23 +11,35 @@ public class KickBall : MonoBehaviour
     public float dribblePower;
     public float shotPower;
 
+    public bool isDribbling = false;
+    Vector3 ballOffset;
+
     // Start is called before the first frame update
     void Start()
     {
         ballrb = ball.GetComponent<Rigidbody>();
+        ballOffset = new Vector3(0f, 1f, 1.8f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(ball.transform.position, transform.position) < kickRadius) {
+            isDribbling = true;
+            ball.transform.position = transform.position + transform.TransformVector(ballOffset);
+        } else {
+            isDribbling = false;
+        }
+
         // Shoot
-        if (Input.GetKeyDown(KeyCode.RightShift) && Vector3.Distance(ball.transform.position, transform.position) < kickRadius) {
+        if (Input.GetKeyDown(KeyCode.RightShift) && isDribbling) {
             Vector3 dir = ball.transform.position - transform.position;
             dir = dir.normalized;
             ballrb.AddForce(dir * shotPower, ForceMode.Impulse);
+            isDribbling = false;
         }
         // Dribble
-        else if (Input.GetKeyDown(KeyCode.Space) && Vector3.Distance(ball.transform.position, transform.position) < kickRadius) {
+        else if (Input.GetKeyDown(KeyCode.Space) && isDribbling) {
             Vector3 dir = ball.transform.position - transform.position;
             dir = dir.normalized;
             ballrb.AddForce(dir * dribblePower, ForceMode.Impulse);
