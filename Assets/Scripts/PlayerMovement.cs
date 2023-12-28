eb7132786ce7c3fd391d6b8d26569db205b2221d
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2023-12-28 23:14:38 +0530

    Fix inverted camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
         if (dir.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime); 
         }
